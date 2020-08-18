bcfb9b45a3515788d22c56b421be32f181a6cecb
Gayathri624 <gayathriselvarajk1969@gmail.com>
2020-08-18 15:45:32 +0530

    commit child1pro

--- enums/src/com/cg/enums/EnumEX.java
+++ enums/src/com/cg/enums/EnumEX.java
@@ -43,6 +43,7 @@ enum  Size
 
            System.out.println("it is large sized pizza");
            System.out.println("it is large sized pizza");
+           System.out.println("it is large sized pizza");
            
            
 
